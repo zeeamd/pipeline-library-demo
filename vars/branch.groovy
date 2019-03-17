db86191e6239a9c747fcf45c13be788d178d3f60
zeeamd <35802043+zeeamd@users.noreply.github.com>
2019-03-17 16:51:27 +0530

    Update branch.groovy

--- vars/branch.groovy
+++ vars/branch.groovy
@@ -3,6 +3,6 @@ package com.cleverbuilder
 
 def call(String name) {
   echo "Branch ${name}"
-  'git clone --branch "${name}" https://github.com/zeeamd/test4jira.git'.execute()
+  echo "git clone --branch "${name}" https://github.com/zeeamd/test4jira.git"
 }
 
