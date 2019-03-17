b02f879bdcd89f0e0375d632f833c682a6490d50
zeeamd <35802043+zeeamd@users.noreply.github.com>
2019-03-17 15:58:43 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,8 +1,9 @@
 #!/usr/bin/env groovy
+package com.cleverbuilder
 
 def call(String name = 'human') {
   echo "Hello, ${name}."
   echo "THIS IS MY GPL"
-  echo "CONSTANT SRC ${GlobalVars.foo}"
+  echo "CONSTANT SRC" GlobalVars.foo
 }
 
