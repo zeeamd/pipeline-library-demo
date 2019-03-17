16728bf95178919e5d15aec71d2afcea3a3648c4
zeeamd <35802043+zeeamd@users.noreply.github.com>
2019-03-17 22:34:04 +0530

    Update testbranch.groovy

--- vars/testbranch.groovy
+++ vars/testbranch.groovy
@@ -3,7 +3,7 @@ package com.cleverbuilder
 
 def call(String name) {
   echo "Branch ${name}"
-  def command = "git clone https://github.com/zeeamd/test4jira.git"
+  def command = "id"
   println ("[$command]")
   Process p = command.execute()
   p.waitFor()               
