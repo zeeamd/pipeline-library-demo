f87e7111bcbffe41c3731a7808148102600e13ea
zeeamd <35802043+zeeamd@users.noreply.github.com>
2019-03-17 22:47:16 +0530

    Update testbranch.groovy

--- vars/testbranch.groovy
+++ vars/testbranch.groovy
@@ -3,19 +3,12 @@ package com.cleverbuilder
 
 def call(String name) {
   echo "Branch ${name}"
-  def c1 = "cd /var/lib/jenkins/workspace/J16-33"
-  def c2 = "git clone https://github.com/zeeamd/test4jira.git"
-  println ("[$c1]")
-  println ("[$c2]")
-  Process p1 = c1.execute()
-  Process p2 = c2.execute()
-  p1.waitFor()
-  p2.waitFor()
-  println "Process exit code: ${p1.exitValue()}"
-  println "Process exit code: ${p2.exitValue()}"
-  println "Std Err: ${p1.err.text}"
-  println "Std Err: ${p2.err.text}"
-  println "Std Out: ${p1.in.text}" 
-  println "Std Out: ${p2.in.text}" 
+  def c = "git clone https://github.com/zeeamd/test4jira.git /var/lib/jenkins/workspace/J16-34"
+  println ("[$c]")
+  Process p = c.execute()
+  p.waitFor()
+  println "Process exit code: ${p.exitValue()}"
+  println "Std Err: ${p.err.text}"
+  println "Std Out: ${p.in.text}"
 }
 
