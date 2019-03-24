25da7af21e73bd33bea9a46634c7dc6f59d19197
zeeamd <35802043+zeeamd@users.noreply.github.com>
2019-03-24 16:16:41 +0530

    Update mavenbuild.groovy

--- vars/mavenbuild.groovy
+++ vars/mavenbuild.groovy
@@ -8,7 +8,7 @@ def call(String[] args) {
         c = c + " " + a;
     }
   println (c);
-  Process p = c.execute(null,'/var/lib/jenkins/workspace/J16-61')
+  Process p = c.execute(null,new File("/var/lib/jenkins/workspace/J16-62"))
   p.waitFor()
   println "Process exit code: ${p.exitValue()}"
   println "Std Out: ${p.err.text}"
