6a143414b46f58ccacb038e348540baab6eaf6d1
zeeamd <35802043+zeeamd@users.noreply.github.com>
2019-03-24 17:12:22 +0530

    Update setcurrentworkspace.groovy

--- vars/setcurrentworkspace.groovy
+++ vars/setcurrentworkspace.groovy
@@ -3,7 +3,7 @@ package com.cleverbuilder
 
 def call(String workspace) {
   echo "Current Workspace ${workspace}"
-  String currentworkspace = ${workspace}
+  String currentworkspace = ${workspace};
   echo "Current Workspace : " currentworkspace
 }
 
