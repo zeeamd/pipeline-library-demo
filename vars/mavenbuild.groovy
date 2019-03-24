541838c1555a8baf4f92f776022a98ec7a865cb0
zeeamd <35802043+zeeamd@users.noreply.github.com>
2019-03-24 16:01:43 +0530

    Update mavenbuild.groovy

--- vars/mavenbuild.groovy
+++ vars/mavenbuild.groovy
@@ -5,12 +5,17 @@ def call(String[] args) {
   echo "Branch ${args[0]}"
   //echo "Clone Dir ${path}"
   //echo "Repo ${}"
-  //def c = "git clone --branch ${bname} ${repo} ${path}"
+  //def c = "mvn ${bname} ${repo} ${path}"
   //println ("[$c]")
   //Process p = c.execute()
   //p.waitFor()
   //println "Process exit code: ${p.exitValue()}"
   //println "Std Out: ${p.err.text}"
   //println "Std Out: ${p.in.text}"
+  for(String a : args) {
+        //using args in above for loop will throw error
+        println (a);
+        println ("\n");
+    }
 }
 
