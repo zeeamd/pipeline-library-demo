bb9bfc4c3c286d30355e7c86360473e9163bb6d6
zee <con.net123@gmail.com>
2019-03-24 15:53:23 +0530

    new build script

--- vars/branch.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-package com.cleverbuilder
-
-def call(String name) {
-  echo "Branch ${name}"
-  //git clone --branch dev 
-  ["git", "clone", "https://github.com/zeeamd/test4jira.git"].execute()
-}
-
---
+++ vars/mavenbuild.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy
+package com.cleverbuilder
+
+def call(String[] args) {
+  echo "Branch ${args[0]}"
+  //echo "Clone Dir ${path}"
+  //echo "Repo ${}"
+  //def c = "git clone --branch ${bname} ${repo} ${path}"
+  println ("[$c]")
+  Process p = c.execute()
+  p.waitFor()
+  println "Process exit code: ${p.exitValue()}"
+  println "Std Out: ${p.err.text}"
+  println "Std Out: ${p.in.text}"
+}
+
