40c42f669cca631faf4e0d38d6ae454232371adc
zeeamd <35802043+zeeamd@users.noreply.github.com>
2019-03-24 16:09:42 +0530

    Update mavenbuild.groovy

--- vars/mavenbuild.groovy
+++ vars/mavenbuild.groovy
@@ -2,17 +2,16 @@
 package com.cleverbuilder
 
 def call(String[] args) {
-  echo "Branch ${args[0]}"
   def c = "mvn"
   for(String a : args) {
         println (a);
         c = c + " " + a;
     }
   println (c);
-  //Process p = c.execute()
-  //p.waitFor()
-  //println "Process exit code: ${p.exitValue()}"
-  //println "Std Out: ${p.err.text}"
-  //println "Std Out: ${p.in.text}"
+  Process p = c.execute()
+  p.waitFor()
+  println "Process exit code: ${p.exitValue()}"
+  println "Std Out: ${p.err.text}"
+  println "Std Out: ${p.in.text}"
 }
 
