1b4dfe0cece41ab8946b45fa216a059ee2bd9494
zeeamd <35802043+zeeamd@users.noreply.github.com>
2019-03-24 15:29:14 +0530

    Update testbranch.groovy

--- vars/testbranch.groovy
+++ vars/testbranch.groovy
@@ -1,9 +1,10 @@
 #!/usr/bin/env groovy
 package com.cleverbuilder
 
-def call(String name) {
-  echo "Branch ${name}"
-  def c = "git clone --branch ${name} https://github.com/zeeamd/test4jira.git"
+def call(String bname,String path) {
+  echo "Branch ${bname}"
+  echo "Clone Dir ${path}"
+  def c = "git clone --branch ${bname} https://github.com/zeeamd/test4jira.git ${path}"
   println ("[$c]")
   Process p = c.execute()
   p.waitFor()
